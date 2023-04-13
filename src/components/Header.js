9023766af5580d538b4bfa24e2d703d5aa7fb623
Bruce <bruce.hillsworth@gmail.com>
2023-04-12 21:05:51 -0600

    Passed tests

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header({ isDarkMode, onDarkModeClick }) {
+	return (
+		<header>
+			<h2>Shopster</h2>
+			<button onClick={onDarkModeClick}>
+				{isDarkMode ? "Light" : "Dark"} Mode
+			</button>
+		</header>
+	);
+}
+
+export default Header;
